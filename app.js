3e972e15b556c48d5be910f0030ac931023b8470
Trey Foster <treybfoster@gmail.com>
2019-07-02 10:41:26 -0500

    Setup for heroku hosting

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("dist"));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("Listening on " + port));
